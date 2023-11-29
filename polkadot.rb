c8bc5e907a17515836a4ac3852a69001c6b2cdf4
Luciano Scarpaci <16402942+lucianoscarpaci@users.noreply.github.com>
2023-11-29 07:19:58 -0500

    Add subkey to show polkadot network

--- polkadot.rb
+++ polkadot.rb
@@ -6,10 +6,7 @@ private_key = RbNaCl::PrivateKey.generate
 private_key_base58 = Base58.binary_to_base58(private_key.to_bytes)
 puts "Private Key: #{private_key_base58}"
 
-
 entropy = private_key_base58.to_s
 mnemonic_words = BipMnemonic.to_mnemonic(mnemonic: entropy)
-puts "Mnemonic: #{mnemonic_words}"
-
-# use the subkey inspect to find public key and address
-# subkey inspect "#{mnemonic_words}"
+subkey = "subkey inspect --network polkadot '#{mnemonic_words}'"
+system(subkey)
